3f65c608220a7473cdafc617d1ac801d55c62f86
RealCarvedArt <78761379+RealCarvedArt@users.noreply.github.com>
2021-04-27 09:30:25 -0400

    Update tesla.rb

--- tesla.rb
+++ tesla.rb
@@ -9,7 +9,7 @@ require 'addressable/uri'
 
 MAX_ATTEMPTS = 10
 CLIENT_ID = '81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384'
-UA = 'Mozilla/5.0 (Linux; Android 10; Pixel 3 Build/QQ2A.200305.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/85.0.4183.81 Mobile Safari/537.36'
+UA = 'Mozilla/5.0 (Linux; Android 11; Pixel 3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Mobile Safari/537.36'
 X_TESLA_USER_AGENT = 'TeslaApp/3.10.9-433/adff2e065/android/10'
 
 def gen_params
